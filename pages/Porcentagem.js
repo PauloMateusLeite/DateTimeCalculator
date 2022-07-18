277a6d9b03553b0a0168f7699902e279f6c539a8
pauloleite_ext <paulo.leite_ext@nexllence.com>
2022-07-18 10:46:38 +0100

    procurando erro

--- pages/Porcentagem.js
+++ pages/Porcentagem.js
@@ -6,39 +6,39 @@ import { Form } from "react-bootstrap";
 
 export default function CalcularPorcentagem(lstCom){
 
-    // if(lstCom[4].ButtonValue == 'Days')
-    // {
-    //   return  PorcentagemDeDias(lstCom); 
-    // }else if(lstCom[4].ButtonValue == 'Hours')
-    // {
-    //   return  PorcentagemDeHoras(lstCom); 
-    // }else if(lstCom[4].ButtonValue == 'Minutes')
-    // {
-    //   return  PorcentagemDeMinutos(lstCom); 
-    // }else 
-    // {
-    //   return  "Erro ao realizar operação de porcentagem.";
-    // }
+    if(lstCom[4].ButtonValue == 'Days')
+    {
+      return  PorcentagemDeDias(lstCom); 
+    }else if(lstCom[4].ButtonValue == 'Hours')
+    {
+      return  PorcentagemDeHoras(lstCom); 
+    }else if(lstCom[4].ButtonValue == 'Minutes')
+    {
+      return  PorcentagemDeMinutos(lstCom); 
+    }else 
+    {
+      return  "Erro ao realizar operação de porcentagem.";
+    }
  
   }
 
   function PorcentagemDeMinutos(lstCom){
-    // let min = PorcentagemDeUmNumero(lstCom[3].ButtonValue,lstCom[0].ButtonValue);
+    let min = PorcentagemDeUmNumero(lstCom[3].ButtonValue,lstCom[0].ButtonValue);
     return  FormatarEmTempo(min);
    }
 
   function PorcentagemDeHoras(lstCom){
-  //  let min = PorcentagemDeUmNumero(HorasEmMinutos(lstCom[3].ButtonValue),lstCom[0].ButtonValue);
+   let min = PorcentagemDeUmNumero(HorasEmMinutos(lstCom[3].ButtonValue),lstCom[0].ButtonValue);
    return  FormatarEmTempo(min);
   }
 
   function PorcentagemDeDias(lstCom){
-    // let min = PorcentagemDeUmNumero(DiasEmMinutos(lstCom[3].ButtonValue),lstCom[0].ButtonValue);
+    let min = PorcentagemDeUmNumero(DiasEmMinutos(lstCom[3].ButtonValue),lstCom[0].ButtonValue);
     return  FormatarEmTempo(min);
   }
 
   function FormatarEmTempo(min){
-    // return  MinutosParaBaseDias(min)+ "d" + MinutosParaBaseHoras(min)+"h"+MinutosParaBaseMinutos(min)+ "m" + MinutosParaBaseSegundos(min) + 's';
+    return  MinutosParaBaseDias(min)+ "d" + MinutosParaBaseHoras(min)+"h"+MinutosParaBaseMinutos(min)+ "m" + MinutosParaBaseSegundos(min) + 's';
   }
 
   function HorasEmMinutos(a){
