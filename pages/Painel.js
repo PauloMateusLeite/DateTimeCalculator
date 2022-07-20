7a615f9212b3080327d5ce0d1ac3682c3e5b5a51
QUICKOPSPC\paulo <paulo.mateus@outlook.com>
2022-07-20 12:53:41 +0100

    +feat before import cdn materialize

--- pages/Painel.js
+++ pages/Painel.js
@@ -1,8 +1,8 @@
 export default function Painel(props){
     return(
-    <div>
-        <div className="row">
-            <div className="col s12 m6">
+    
+        
+            <div className="col s12 m12">
                 <div className="card blue-grey darken-1">
                     <div className="card-content white-text">
                         <span className="card-title">{props.Result} </span>
@@ -13,7 +13,7 @@ export default function Painel(props){
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+       
+    
     );
 }
